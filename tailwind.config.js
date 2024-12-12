ebb92843015ee6b8c354ea57bab0855082c8d5ba
Raouane <raouanetransport@gmail.com>
2024-12-12 13:39:47 +0100

    style: update color scheme to lighter shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        yellow: {
-          DEFAULT: '#F4B41A', // Jaune MonHajj
-          500: '#F4B41A',
-          600: '#E5A918',
-          700: '#D69E16',
+        'black': {
+          DEFAULT: 'rgb(32, 78, 94)',
+          600: 'rgb(32, 78, 94)',
+          700: 'rgb(28, 68, 82)',
         },
-        black: {
-          DEFAULT: '#143642', // Noir MonHajj
-          500: '#143642',
-          600: '#0F2931',
-          700: '#0A1C20',
-        }
+        'yellow': {
+          DEFAULT: '#FFD700',
+          600: '#FFE44D',
+        },
       },
     },
   },
